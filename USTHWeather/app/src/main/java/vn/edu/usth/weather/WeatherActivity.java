4fffe78dacc9a7fe7353902c897f5fde219c78c3
Lê Nhật Anh <lnafncy20@gmail.com>
2024-09-28 10:54:53 +0700

    Submit for Practical work 11

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,6 +1,8 @@
 package vn.edu.usth.weather;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
@@ -10,6 +12,7 @@ import com.google.android.material.tabs.TabLayoutMediator;
 
 public class WeatherActivity extends AppCompatActivity {
     private static final String TAG = "WeatherActivity";
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +40,25 @@ public class WeatherActivity extends AppCompatActivity {
                             break;
                     }
                 }).attach();
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.music);  // Replace 'your_music_file' with your actual MP3 file in res/raw
+
+        // Start playing music as soon as the activity is created
+        if (mediaPlayer != null) {
+            mediaPlayer.start();  // Automatically start playing the music
+            Log.i(TAG, "Music started playing automatically");
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // Release the MediaPlayer when the activity is destroyed
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+            Log.i(TAG, "MediaPlayer resources released");
+        }
     }
 
     @Override
@@ -61,11 +83,5 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "onStop called");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i(TAG, "onDestroy called");
     }
 }
